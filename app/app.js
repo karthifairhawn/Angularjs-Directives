3ced96884b278a6c462a9eb57cb819cd95484029
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-04-19 17:08:09 +0530

    End of ng-click

--- app/app.js
+++ app/app.js
@@ -3,6 +3,23 @@ var app = angular.module('app', [] );
 
 app.controller('app-controller-1', ['$scope', function($scope){
 
+    $scope.removeItem = function(item){
+        var removing_item_index = $scope.messages.indexOf(item);
+        $scope.messages.splice(removing_item_index,1);
+    };
+
+    $scope.add_message = function(){
+        $scope.messages.push({
+            name: $scope.new_item.name,
+            mark: parseInt($scope.new_item.mark),
+            color: $scope.new_item.color,
+            available : true
+        });
+
+        $scope.new_item.name = "";
+        $scope.new_item.mark = "";
+        $scope.new_item.color = "";
+    };
     $scope.messages = [
         {
             name: 'moni',
@@ -14,12 +31,12 @@ app.controller('app-controller-1', ['$scope', function($scope){
         name: 'karthi',
         color: 'green',
         mark: 90,
-        available: false
+        available: true
     },
     {
         name: 'monica',
         color: 'red',
-        mark: 100,
+        mark: 80,
         available: true
     }
 ];
